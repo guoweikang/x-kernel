f506efe5b1cc099b308d77679ef884269a44d0eb
bigfish <thebigfish@foxmail.com>
2026-02-05 17:36:35 +0800

    tee: add tee support for storage and alg (#106)
    * tee: add tee support
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * tee: add cfg_if check for aarch64 tee syscall.
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * tee: fix tee_scn_storage_obj_create syscall compile error
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * test
    
    * 1
    
    * 1
    
    * tee: delete kapi/tee_test dep
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * tee: delete kapi/tee_test dep
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * set musl toolchain for ci
    
    * tee: delete kapi/tee_test dep
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * clippy
    
    * clippy
    
    * add cargo fmt
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * add cargo fmt
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * add cargo fmt
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    * add cargo fmt
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    
    ---------
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>
    Co-authored-by: 于博 <yubo1@kylinos.cn>
    Co-authored-by: wangyining <wangyining@kylinos.cn>

---
+++ api/kapi/src/tee/crypto_temp/mod.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+
+// NOTE: !!!Temporary file for crypto operations.!!!
+//       !!! DO NOT ADD NEW FUNCTIONS TO THIS FILE !!!
+// TODO: Remove this file after the crypto module are implemented.
+
+pub mod aes_cbc;
+pub mod crypto_hash_temp;
+pub mod hash;
---
+++ api/kapi/src/tee/user_ta.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+
+use core::default::Default;
+
+/// user ta context
+/// NOTE: NEVER USE THIS STRUCT IN YOUR CODE
+#[repr(C)]
+#[derive(Default)]
+pub struct user_ta_ctx {}
